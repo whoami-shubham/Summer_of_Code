6a7d351803fd5a28c3fdf524818d7d954bae6f67
whoami-shubham <shubhamkumarjha0013@gmail.com>
2019-03-17 00:28:34 +0530

    all files added first time

---
+++ precache-manifest.3555dddca9d82d6c1ac1bf301861bd96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18758d7980354d39504d4ee4117ef5d1",
+    "url": "/static/media/logo.18758d79.png"
+  },
+  {
+    "revision": "d576c0a33d199b74dd90081301e1eae5",
+    "url": "/static/media/home3.d576c0a3.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "642920e795b1a30eef1f",
+    "url": "/static/js/main.73d31180.chunk.js"
+  },
+  {
+    "revision": "f5d0e552dda36c2098b3",
+    "url": "/static/js/2.f730f75c.chunk.js"
+  },
+  {
+    "revision": "17a92cfd7b0e17092eaab57fb931c624",
+    "url": "/index.html"
+  }
+];
